b5c3801d5e9f5afb26ce42888315b3e098cc172b
pandoragear <andy.dang.it@gmail.com>
2021-02-26 23:00:08 -0600

    Add futureDate time stamp

--- app.js
+++ app.js
@@ -23,8 +23,11 @@ const weekdays = [
     "Friday", 
 ];
 
+// defining the name to variable 
 const dayLeft = document.querySelector(".dayLeft");
 const deadline = document.querySelector(".deadline");
 const items =  document.querySelectorAll(".deadline-format h4")
 
-console.log(items);  
+//Date
+let futureDate = new Date(2021,11,25,00,00,01);
+console.log(futureDate);
